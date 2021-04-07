434ff5ac103c4520b3fbb662f9032ff4f56ca040
Russ Miles <russ@russmiles.com>
2021-04-07 10:14:06 +0100

    Added template service implementation
    Contributes to #1
    
    Signed-off-by: Russ Miles <russ@russmiles.com>

---
+++ src/main/java/com/reliably/randomlatencyinducer/RandomLatencyInducerApplication.java
@@ -0,0 +1,13 @@
+package com.reliably.randomlatencyinducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomLatencyInducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomLatencyInducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reliably/randomlatencyinducer/RandomLatencyInducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reliably.randomlatencyinducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomLatencyInducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
