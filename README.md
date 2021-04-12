9358fca8e94c50f0d0d76e8e4fc10aa46434b696
Russ Miles <russ@russmiles.com>
2021-04-12 15:01:03 +0100

    Added http endpoint
    Contributes to #1
    
    Signed-off-by: Russ Miles <russ@russmiles.com>

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # randomlatencyinducer
 A sample Random Latency Inducer service.
+
+## Building the service
+
+This service is built using [Maven][mvn]. To build the service, run the tests and package ready for distributing using  Docker use the following command:
+
+[mvn]:
+
+```shell
+./mvnw package
+```
+
+## Build a Docker image for the service
+
+To build a Docker image for distribution of the service, run the :
+```shell
+docker build -t reliably/randomlatencyinjector .
+```
+
+## Run the service using Docker
+
+```shell
+docker run -p 8080:8080 reliably/randomlatencyinjector
+```
--- src/main/java/com/reliably/randomlatencyinducer/RandomLatencyInducerApplication.java
+++ src/main/java/com/reliably/randomlatencyinducer/RandomLatencyInducerApplication.java
@@ -2,12 +2,19 @@ package com.reliably.randomlatencyinducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RandomLatencyInducerApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Am I delayed??";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RandomLatencyInducerApplication.class, args);
 	}
-
 }
